50a931236f5a01081fea6abe8d0f5054c876d9f8
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-04-03 11:45:03 +0800

    Updated Requirements Per User views and CRUD

--- application/rbaconsultancy_adv/backend/views/requirementsperuser/index.php
+++ application/rbaconsultancy_adv/backend/views/requirementsperuser/index.php
@@ -1,7 +1,11 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\helpers\ArrayHelper;
+use common\models\RequirementsList;
+use common\models\UserMain;
+use common\models\ServiceList;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\RequirementsPerUserSearch */
@@ -12,29 +16,101 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="requirements-per-user-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php 
 
-    <p>
-        <?= Html::a('Create Requirements Per User', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    $gridColumns = [
+        [
+            'class'=>'kartik\grid\SerialColumn',
+            'contentOptions'=>['class'=>'kartik-sheet-style'],
+            'header'=>'',
+            'headerOptions'=>['class'=>'kartik-sheet-style']
+        ],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+        [
+            'attribute'=>'user_id', 
+            'width'=>'250px',
+             'value'=>function ($model, $key, $index, $widget) { 
+                return Html::a($model->user->username,  
+                    '#', 
+                    ['title'=>'View author detail', 'onclick'=>'alert("This will open the author page.\n\nDisabled for this demo!")']);
+            },
+            'vAlign'=>'middle',
+            'filterType'=>GridView::FILTER_SELECT2,
+            'filter'=>ArrayHelper::map(UserMain::find()->orderBy('username')->asArray()->all(), 'id', 'username'), 
+            'filterWidgetOptions'=>[
+                'pluginOptions'=>['allowClear'=>true],
+                ],
+            'filterInputOptions'=>['placeholder'=>'Select Client'],
+            'format'=>'raw',
+            'label' => 'Acquired by',
+        ],
 
-            //'rpu_id',
-            'rlist_id', //requirement name
-            'user_id', //submitted by
-            'service_id', //service name
+       [
+            'attribute'=>'service_id', 
+            'width'=>'250px',
+             'value'=>function ($model, $key, $index, $widget) { 
+                return Html::a($model->slist->slist_name,  
+                    '#', 
+                    ['title'=>'View author detail', 'onclick'=>'alert("This will open the author page.\n\nDisabled for this demo!")']);
+            },
+            'vAlign'=>'middle',
+            'filterType'=>GridView::FILTER_SELECT2,
+            'filter'=>ArrayHelper::map(ServiceList::find()->orderBy('slist_name')->asArray()->all(), 'slist_id', 'slist_name'), 
+            'filterWidgetOptions'=>[
+                'pluginOptions'=>['allowClear'=>true],
+                ],
+            'filterInputOptions'=>['placeholder'=>'Select Service'],
+            'format'=>'raw',
+            'label' => 'Service',
+        ],
+
+        [
+            'attribute'=>'rlist_id', 
+            'width'=>'250px',
+             'value'=>function ($model, $key, $index, $widget) { 
+                return Html::a($model->requirementslist->rlist_name,  
+                    '#', 
+                    ['title'=>'View author detail', 'onclick'=>'alert("This will open the author page.\n\nDisabled for this demo!")']);
+            },
+            'vAlign'=>'middle',
+            'filterType'=>GridView::FILTER_SELECT2,
+            'filter'=>ArrayHelper::map(RequirementsList::find()->orderBy('rlist_name')->asArray()->all(), 'rlist_id', 'rlist_name'), 
+            'filterWidgetOptions'=>[
+                'pluginOptions'=>['allowClear'=>true],
+                ],
+            'filterInputOptions'=>['placeholder'=>'Select Requirements'],
+            'format'=>'raw',
+        ],
+
             'rpu_datefilesubmitted', //date
             'rpu_fileuploaded', //uploaded files, if applicable
             'rpu_status',
 
-            ['class' => 'yii\grid\ActionColumn'],
+
+        ['class' => 'kartik\grid\ActionColumn'],
+    ];
+
+    echo GridView::widget([
+        'dataProvider'=> $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => $gridColumns,
+        'responsive'=>true,
+        'hover'=>true,
+
+        'containerOptions' => ['style'=>'overflow: auto'], // only set when $responsive = false
+        'headerRowOptions'=>['class'=>'kartik-sheet-style'],
+        'filterRowOptions'=>['class'=>'kartik-sheet-style'],
+        'pjax' => true, 
+        'floatHeader'=>true,
+        'panel' => [
+            'heading'=>'<h3 class="panel-title"><i class="glyphicon glyphicon-list-alt"></i>&nbsp&nbsp Requirements Per User</h3>',
+            'type'=>GridView::TYPE_PRIMARY,
+            'before'=>Html::tag('p', '*This is auto-generated when a client applied for Visa.'),
+            'after'=>Html::a('<i class="glyphicon glyphicon-repeat"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
         ],
-    ]); ?>
+
+    ]); 
+
+    ?>
 
 </div>

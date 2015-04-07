06f47fcfef3aada5479d066dfab946269e7c2b5b
Kayzelle Gabalfin <kayzellegabalfin@gmail.com>
2015-04-07 23:53:56 +0800

    Updated User views and CRUD

--- application/rbaconsultancy_adv/backend/views/site/index.php
+++ application/rbaconsultancy_adv/backend/views/site/index.php
@@ -5,14 +5,6 @@ $this->title = 'RBA Consultancy and General Services';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
 
         <div class="row">
